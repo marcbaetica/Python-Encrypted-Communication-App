d894ffb317b0e63dcd57ae65328e760c7cf4f3f6
Marc Baetica <marc.baetica@gmail.com>
2021-11-13 09:13:29 -0500

    Mapping message sizes for fist message recv, incoming payloads permutations doc.

--- lib/comms_protocol.py
+++ lib/comms_protocol.py
@@ -9,7 +9,8 @@ from lib.utils import decode_and_remove_padding, encode_and_apply_padding
 load_dotenv()
 
 PADDED_MESSAGE_SIZE = int(os.getenv('PADDED_MESSAGE_SIZE'))
-ACK_CODE = os.getenv('ACK_CODE')
+ACK_SIZE_CODE = os.getenv('ACK_SIZE_CODE')
+ACK_MESSAGE_CODE = os.getenv('ACK_MESSAGE_CODE')
 EXIT_CODE = os.getenv('EXIT_CODE')
 
 
@@ -33,10 +34,26 @@ class CommsProtocolHandler:
 
     @staticmethod
     def receive_data_attempt(socket, who):  # TODO: handle retries here.
+        """Can receive:
+         - nothing / blocked call -> idling means socket is still open and pending network buffer fill-in
+         - instant empty bytes object (encoded message string) on all calls  -> other party socket has been closed
+         - incoming message size (padded) -> message is about to be send, pending confirmation
+         - message size confirmation -> confirms that message has been received, therefore waiting for the message
+         - actual message
+         - confirmation of message received -> without this the other party will retry to sent the message
+
+        :param socket: (Socket) Socket object to listen to.
+        :param who: (String) Entity listening to this call. Used for logging. Can be either 'Server' or 'Client'.
+        :return:
+        """
         if who not in ['Server', 'Client']:
             raise ValueError(f'Parameter destination can only have values "Server" or "Client". Value received: {who}')
         try:
-            payload = socket.recv(PADDED_MESSAGE_SIZE)
+            potential_messages = [ACK_MESSAGE_CODE, ACK_SIZE_CODE, ACK_MESSAGE_CODE, EXIT_CODE]
+            potential_message_sizes = [len(message) for message in potential_messages]
+            max_message_size = max(potential_message_sizes)
+            print(max_message_size)
+            payload = socket.recv(max_message_size)
             if payload == b'':  # Client socket closed due to no incoming data.
                 return payload
         except ConnectionResetError:
@@ -44,20 +61,20 @@ class CommsProtocolHandler:
             socket.close()
             return
         print(payload)
-        if payload.decode() == ACK_CODE:
+        if payload.decode() == ACK_MESSAGE_CODE:
             pass
         else:
             incoming_payload_length = decode_and_remove_padding(payload)
             print(f'{who} received size of next message: "{incoming_payload_length}"')
-            print(ACK_CODE)  # TODO: handle this!
-            socket.send(ACK_CODE.encode())
+            print(ACK_MESSAGE_CODE)  # TODO: handle this!
+            socket.send(ACK_MESSAGE_CODE.encode())
             payload = socket.recv(incoming_payload_length).decode()
             return payload
 
     @classmethod
     def _receive_confirmation(cls, socket):
-        confirmation = socket.recv(len(ACK_CODE))
-        return True if confirmation == ACK_CODE.encode() else False
+        confirmation = socket.recv(len(ACK_MESSAGE_CODE))
+        return True if confirmation == ACK_MESSAGE_CODE.encode() else False
 
     @staticmethod
     def is_close_socket_attempt(message, address, who):
