3905c10150094b24822dc0c1f11b796f03c2d8c9
Marc Baetica <marc.baetica@gmail.com>
2021-11-15 20:28:59 -0500

    Preliminary send message attempt implementation.

--- communication_protocols/comms_protocol_advanced_handler.py
+++ communication_protocols/comms_protocol_advanced_handler.py
@@ -8,14 +8,28 @@ class AdvancedCommsProtocolHandler:
         self.comms_protocol = AdvancedCommsProtocol(socket)
 
     def send_message_attempt(self, message):
-        self.comms_protocol.send_incoming_payload_size(message)
-        if not self.comms_protocol.is_message_size_confirmation_received():
-            # if no confirmation, re-send length, repeat 10 times. If still no result, raise conn failed + close socket
-            pass  # TODO: what if it doesn't arrive on time?
-        self.comms_protocol.send_message(message)
-        if not self.comms_protocol.is_message_size_confirmation_received():
-            # if no confirmation, re-send message, repeat 10 times. If still no result, raise conn failed + close socket
-            pass  # TODO: what if it doesn't arrive on time?
+        try:
+            self.comms_protocol.send_incoming_payload_size(message)
+            if not self.comms_protocol.is_message_size_confirmation_received():
+                # if no confirmation, re-send length, repeat 10 times. If still no result, raise conn failed + close socket
+                print('Payload message size confirmation not received...')
+                pass  # TODO: what if it doesn't arrive on time?
+            self.comms_protocol.send_message(message)
+            if not self.comms_protocol.is_message_confirmation_received():
+                # if no confirmation, re-send message, repeat 10 times. If still no result, raise conn failed + close socket
+                print('Payload message confirmation not received...')
+                pass  # TODO: what if it doesn't arrive on time?
+        except ConnectionError as e:
+            # Python socket behavior is expected to hang upon attempting to read from the network buffer.
+            # If other party socket is closed, the network buffer receive method call returns empty byte array.
+            # Hence, socket needs to be closed.
+            self.socket.close()
+            print(f'Connection error: {e}')
+        except OSError as e:
+            # When socket has been terminated, all subsequent operations involving the object should simply break.
+            # Specific scenario logging has been accounted for.
+            # OSError: [WinError 10038] An operation was attempted on something that is not a socket.
+            print('Socket has been closed. Application terminating.')
 
     def receive_message_attempt(self):
         try:
@@ -30,9 +44,10 @@ class AdvancedCommsProtocolHandler:
             # Hence, socket needs to be closed.
             self.socket.close()
             print(f'Connection error: {e}')
-            return
         except OSError as e:
             # When socket has been terminated, all subsequent operations involving the object should simply break.
             # Specific scenario logging has been accounted for.
-            # OSError: [WinError 10038] An operation was attempted on something that is not a socket
-            self.socket.close()
+            # OSError: [WinError 10038] An operation was attempted on something that is not a socket.
+            print('Socket has been closed. Application terminating.')
+
+# TODO: If no expected message, re-send, repeat 10 times. If still no result, raise conn failed + close socket.
